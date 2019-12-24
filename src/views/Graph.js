9b4a0fed2622b8863e557230b95a297e7d9cf5ce
Harshit Maurya <hmaurya999@gmail.com>
2019-12-24 19:27:36 +0530

    create basic line graph

--- src/views/Graph.js
+++ src/views/Graph.js
@@ -1,14 +1,34 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {LineChart, Grid} from 'react-native-svg-charts';
 
 export default class Graph extends Component {
   render() {
+    const data = [
+      50,
+      10,
+      40,
+      95,
+      -4,
+      -24,
+      85,
+      91,
+      35,
+      53,
+      -53,
+      24,
+      50,
+      -20,
+      -80,
+    ];
+
     return (
-      <>
-        <View>
-          <Text>Insert Graph Here</Text>
-        </View>
-      </>
+      <LineChart
+        style={{height: 200}}
+        data={data}
+        svg={{stroke: 'rgb(134, 65, 244)'}}
+        contentInset={{top: 20, bottom: 20}}>
+        <Grid />
+      </LineChart>
     );
   }
 }
