eaf5ab284d7472575e9ca1a81ad572dec28c750a
Harshit Maurya <hmaurya999@gmail.com>
2019-12-24 17:13:38 +0530

    Initial app setup

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <View>
+        <Text>Charts Comparison</Text>
+      </View>
+    </>
+  );
+};
+
+export default App;
