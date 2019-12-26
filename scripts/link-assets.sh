50bd7c2cfa337f75d4124d59a5226a50aa4c25b8
Harshit Maurya <hmaurya999@gmail.com>
2019-12-26 20:07:30 +0530

    fix symlink script

--- scripts/link-assets.sh
+++ scripts/link-assets.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-mkdir -p ../android/app/src/main/assets
-mkdir -p ../ios/assets
-CHART_JS_LIB=$(pwd)/../node_modules/chart.js/dist/Chart.min.js
-ln -s $CHART_JS_LIB ../android/app/src/main/assets/
-ln -s $CHART_JS_LIB ../ios/assets/
+BASEDIR=$(pwd)
+ANDROID_ASSETS_DIR=$BASEDIR/android/app/src/main/assets
+IOS_ASSETS_DIR=$BASEDIR/ios/assets
+CHART_JS_LIB=$BASEDIR/node_modules/chart.js/dist/Chart.min.js
+rm -rf $ANDROID_ASSETS_DIR $IOS_ASSETS_DIR
+mkdir -p $ANDROID_ASSETS_DIR
+mkdir -p $IOS_ASSETS_DIR
+ln -s $CHART_JS_LIB $ANDROID_ASSETS_DIR
+ln -s $CHART_JS_LIB $IOS_ASSETS_DIR
